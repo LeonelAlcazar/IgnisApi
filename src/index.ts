04476429d989fc2a8f9fa3509188b73af3c5f39a
YLi Alex <alex@hks.ar>
2023-10-07 15:57:06 -0300

    feature: twilio notification

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,32 @@ import axios from "axios";
 import dotenv from "dotenv";
 import { db } from "./firebase";
 import { CronJob } from "cron";
+import express from "express";
+import twilio from "twilio";
+const client = twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
 
 dotenv.config();
+
+type Fire = {
+  latitude: number;
+  longitude: number;
+  bright_ti4: number;
+  scan: number;
+  track: number;
+  confidence: number;
+  bright_ti5: number;
+  frp: number;
+  country_id: string;
+  acq_date: string;
+  acq_time: string;
+  satellite: string;
+  instrument: string;
+  version: string;
+  daynight: string;
+};
 
 function parseCSV<T>(csv: string): T[] {
   const lines = csv.split("\n");
@@ -79,6 +103,91 @@ async function deleteCollection() {
   }
 }
 
+type InterestPoint = {
+  label: string;
+  lat: number;
+  lng: number;
+  radius: number;
+  userId: string;
+};
+
+async function NotifyUsers(userFires: {
+  [userId: string]: { point: InterestPoint; fires: Fire[] }[];
+}) {
+  const userIds = Object.keys(userFires);
+  // Get from collection "phones" the phone number of each user (the userdId is the same id of the document)
+  const phonesRef = db.collection("phones");
+  const phones: { [userId: string]: string } = {};
+  for (const userId of userIds) {
+    const doc = await phonesRef.doc(userId).get();
+    if (doc.exists) {
+      phones[userId] = doc.data()?.phone;
+
+      // Send a message to each user
+      const fires = userFires[userId];
+      let message = "Incendio cerca de " + fires[0].point.label + "\n";
+
+      try {
+        client.messages.create({
+          body: message,
+          from: process.env.TWILIO_PHONE_NUMBER,
+          to: phones[userId],
+        });
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }
+
+  // Send a message to each user
+}
+
+async function analyzeFires(fires: Fire[]) {
+  try {
+    // Get collection InterestPoints
+    const interestPointsRef = db.collection("interestPoints");
+    const querySnapshot = await interestPointsRef.get();
+    const interestPoints: InterestPoint[] = [];
+    querySnapshot.forEach((doc) => {
+      interestPoints.push(doc.data() as InterestPoint);
+    });
+
+    const notificationToUser: {
+      [key: string]: { point: InterestPoint; fires: Fire[] }[];
+    } = {};
+
+    // For each interest point
+    interestPoints.forEach((interestPoint) => {
+      // Get fires in radius
+      const firesInRadius = fires.filter((fire) => {
+        const distance =
+          Math.acos(
+            Math.sin(fire.latitude) * Math.sin(interestPoint.lat) +
+              Math.cos(fire.latitude) *
+                Math.cos(interestPoint.lat) *
+                Math.cos(fire.longitude - interestPoint.lng)
+          ) * 6371;
+        return distance <= interestPoint.radius;
+      });
+
+      if (firesInRadius.length > 0) {
+        if (!notificationToUser[interestPoint.userId]) {
+          notificationToUser[interestPoint.userId] = [];
+        }
+        notificationToUser[interestPoint.userId].push({
+          point: interestPoint,
+          fires: firesInRadius,
+        });
+      }
+    });
+
+    // Send notification to users
+    await NotifyUsers(notificationToUser);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
 async function SaveFires() {
   try {
     console.log("Give me the fires!");
@@ -97,7 +206,13 @@ async function SaveFires() {
     });
 
     await writeBatch.commit();
+
     console.log("Fires saved!");
+    try {
+      await analyzeFires(data);
+    } catch (e) {
+      console.error(e);
+    }
   } catch (e) {
     console.error(e);
     await SaveFires();
@@ -107,3 +222,5 @@ async function SaveFires() {
 // Every 30 minutes
 SaveFires();
 const job = new CronJob("*/30 * * * *", SaveFires, null, true);
+
+const app = express();
